23343665cd1f45551a1389676cd73b66a1ced92f
GuidoZyl <guidozyl@gmail.com>
2021-10-18 19:42:09 -0300

    un par de cambios

--- Letea Pantallas Juntas/FormPacientes.Designer.cs
+++ Letea Pantallas Juntas/FormPacientes.Designer.cs
@@ -35,11 +35,13 @@ namespace Pantalla_Contraseña
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(147, 348);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(500, 400);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(365, 54);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Uriel Jolodovsky";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormPacientes
             // 
