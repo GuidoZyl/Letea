0266f075eceebefe87a9aa1d7f6d295f08a1a42b
GuidoZyl <guidozyl@gmail.com>
2021-10-24 01:23:10 -0300

    asd

---
+++ Letea Pantallas Juntas/Elegir Paciente 2.0.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pantalla_Contraseña
+{
+    public partial class FormPacientes2 : Form
+    {
+        public FormPacientes2()
+        {
+            InitializeComponent();
+        }
+
+        private void Elegir_Paciente_2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
