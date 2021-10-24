46e487c3c1ffe64cf054faf4df3126a69df93002
Uriel Jolodosvky <urieljolo@gmail.com>
2021-10-24 02:47:02 -0300

    Flechita Izquierda

--- Letea Pantallas Juntas/Elegir Paciente 2.0.cs
+++ Letea Pantallas Juntas/Elegir Paciente 2.0.cs
@@ -113,10 +113,12 @@ namespace Pantalla_Contraseña
             {
                 btn_Der.Visible = false;
             }
+
         }
 
         private void btn_Der_Click(object sender, EventArgs e)
         {
+            
             int z = 0;
 
             if (lbl_Nom4.Text != null)
@@ -216,111 +218,38 @@ namespace Pantalla_Contraseña
                             lbl_Nom5.Text = nombrecompleto[ultimo, 0];
                         }
                     }
+                    if (lbl_Nom1.Text != nombrecompleto[0, 0])
+                    {
+                        btn_Izq.Visible = true;
+                    }
                 }
             }
         }
 
         private void btn_Izq_Click(object sender, EventArgs e)
         {
-            int z = 0;
 
             if (lbl_Nom1.Text != nombrecompleto[0, 0])
             {
+                
                 for (int i = 4; i <= ultimo; i++)
                 {
-                    if (z == 0)
-                    {
-                        if (lbl_Nom4.Text == nombrecompleto[i, 0])
+                    
+                        if (lbl_Nom1.Text == nombrecompleto[i, 0] || lbl_Nom5.Text == nombrecompleto[i, 0] || lbl_Nom6.Text == nombrecompleto[i, 0])
                         {
-                            lbl_Nom1.Text = nombrecompleto[i + 1, 0];
-                            lbl_Nom2.Text = nombrecompleto[i + 2, 0];
-                            lbl_Nom3.Text = nombrecompleto[i + 3, 0];
-                            lbl_Nom4.Text = nombrecompleto[i + 4, 0];
+                            lbl_Nom1.Text = nombrecompleto[i - 4, 0];
+                            lbl_Nom2.Text = nombrecompleto[i - 3, 0];
+                            lbl_Nom3.Text = nombrecompleto[i - 2, 0];
+                            lbl_Nom4.Text = nombrecompleto[i - 1, 0];
+                            
 
-                            z = 1;
 
-                            if (lbl_Nom4.Text == nombrecompleto[ultimo, 0])
+                        if (lbl_Nom1.Text == nombrecompleto[0,0])
                             {
-                                btn_Der.Visible = false;
+                                btn_Izq.Visible = false;
                             }
                         }
-                    }
-
-                    if (z == 0)
-                    {
-                        if (lbl_Nom4.Text == nombrecompleto[ultimo - 3, 0] && (ultimo + 1) % 4 == 3)
-                        {
-
-                            lbl_Nom3.Visible = false;
-                            lbl_Nom4.Visible = false;
-                            lbl_Nom5.Visible = false;
-                            lbl_Nom6.Visible = false;
-                            lbl_Nom7.Visible = false;
-                            lbl_Nom1.Visible = true;
-                            lbl_Nom2.Visible = true;
-                            lbl_Nom8.Visible = true;
-                            pic_Nom3.Visible = false;
-                            pic_Nom4.Visible = false;
-                            pic_Nom5.Visible = false;
-                            pic_Nom6.Visible = false;
-                            pic_Nom7.Visible = false;
-                            pic_Nom1.Visible = true;
-                            pic_Nom2.Visible = true;
-                            pic_Nom8.Visible = true;
-                            btn_Der.Visible = false;
-
-                            lbl_Nom1.Text = nombrecompleto[ultimo - 2, 0];
-                            lbl_Nom2.Text = nombrecompleto[ultimo - 1, 0];
-                            lbl_Nom8.Text = nombrecompleto[ultimo, 0];
-                        }
-
-                        else if (lbl_Nom4.Text == nombrecompleto[ultimo - 2, 0] && (ultimo + 1) % 4 == 2)
-                        {
-                            lbl_Nom3.Visible = false;
-                            lbl_Nom4.Visible = false;
-                            lbl_Nom5.Visible = false;
-                            lbl_Nom6.Visible = true;
-                            lbl_Nom7.Visible = true;
-                            lbl_Nom1.Visible = false;
-                            lbl_Nom2.Visible = false;
-                            lbl_Nom8.Visible = false;
-                            pic_Nom3.Visible = false;
-                            pic_Nom4.Visible = false;
-                            pic_Nom5.Visible = false;
-                            pic_Nom6.Visible = true;
-                            pic_Nom7.Visible = true;
-                            pic_Nom1.Visible = false;
-                            pic_Nom2.Visible = false;
-                            pic_Nom8.Visible = false;
-                            btn_Der.Visible = false;
-
-                            lbl_Nom6.Text = nombrecompleto[ultimo - 1, 0];
-                            lbl_Nom7.Text = nombrecompleto[ultimo, 0];
-                        }
-
-                        else if (lbl_Nom4.Text == nombrecompleto[ultimo - 1, 0] && (ultimo + 1) % 4 == 1)
-                        {
-                            lbl_Nom3.Visible = false;
-                            lbl_Nom4.Visible = false;
-                            lbl_Nom5.Visible = true;
-                            lbl_Nom6.Visible = false;
-                            lbl_Nom7.Visible = false;
-                            lbl_Nom1.Visible = false;
-                            lbl_Nom2.Visible = false;
-                            lbl_Nom8.Visible = false;
-                            pic_Nom3.Visible = false;
-                            pic_Nom4.Visible = false;
-                            pic_Nom5.Visible = true;
-                            pic_Nom6.Visible = false;
-                            pic_Nom7.Visible = false;
-                            pic_Nom1.Visible = false;
-                            pic_Nom2.Visible = false;
-                            pic_Nom8.Visible = false;
-                            btn_Der.Visible = false;
-
-                            lbl_Nom5.Text = nombrecompleto[ultimo, 0];
-                        }
-                    }
+                    
                 }
             }
         }
