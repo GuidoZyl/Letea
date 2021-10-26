678965b39febff81c3cf6ab42787f6af76141787
GuidoZyl <guidozyl@gmail.com>
2021-10-25 22:06:53 -0300

    asd

---
+++ Letea Pantallas Juntas/Grupos Amigos.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Pantalla_Contraseña
+{
+    partial class FormGruposAmigos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic_Nom1 = new System.Windows.Forms.PictureBox();
+            this.pic_Nom2 = new System.Windows.Forms.PictureBox();
+            this.pic_Nom3 = new System.Windows.Forms.PictureBox();
+            this.pic_Nom4 = new System.Windows.Forms.PictureBox();
+            this.pic_Nom5 = new System.Windows.Forms.PictureBox();
+            this.pic_Nom6 = new System.Windows.Forms.PictureBox();
+            this.pic_Nom7 = new System.Windows.Forms.PictureBox();
+            this.pic_Nom8 = new System.Windows.Forms.PictureBox();
+            this.lbl_Nom1 = new System.Windows.Forms.Label();
+            this.lbl_Nom2 = new System.Windows.Forms.Label();
+            this.lbl_Nom3 = new System.Windows.Forms.Label();
+            this.lbl_Nom4 = new System.Windows.Forms.Label();
+            this.lbl_Nom5 = new System.Windows.Forms.Label();
+            this.lbl_Nom6 = new System.Windows.Forms.Label();
+            this.lbl_Nom7 = new System.Windows.Forms.Label();
+            this.lbl_Nom8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_Nom1
+            // 
+            this.pic_Nom1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_Nom1.Location = new System.Drawing.Point(500, 70);
+            this.pic_Nom1.Name = "pic_Nom1";
+            this.pic_Nom1.Size = new System.Drawing.Size(300, 300);
+            this.pic_Nom1.TabIndex = 9;
+            this.pic_Nom1.TabStop = false;
+            // 
+            // pic_Nom2
+            // 
+            this.pic_Nom2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_Nom2.Location = new System.Drawing.Point(1150, 70);
+            this.pic_Nom2.Name = "pic_Nom2";
+            this.pic_Nom2.Size = new System.Drawing.Size(300, 300);
+            this.pic_Nom2.TabIndex = 10;
+            this.pic_Nom2.TabStop = false;
+            // 
+            // pic_Nom3
+            // 
+            this.pic_Nom3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_Nom3.Location = new System.Drawing.Point(500, 550);
+            this.pic_Nom3.Name = "pic_Nom3";
+            this.pic_Nom3.Size = new System.Drawing.Size(300, 300);
+            this.pic_Nom3.TabIndex = 11;
+            this.pic_Nom3.TabStop = false;
+            // 
+            // pic_Nom4
+            // 
+            this.pic_Nom4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_Nom4.Location = new System.Drawing.Point(1150, 550);
+            this.pic_Nom4.Name = "pic_Nom4";
+            this.pic_Nom4.Size = new System.Drawing.Size(300, 300);
+            this.pic_Nom4.TabIndex = 12;
+            this.pic_Nom4.TabStop = false;
+            // 
+            // pic_Nom5
+            // 
+            this.pic_Nom5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_Nom5.Location = new System.Drawing.Point(800, 250);
+            this.pic_Nom5.Name = "pic_Nom5";
+            this.pic_Nom5.Size = new System.Drawing.Size(300, 300);
+            this.pic_Nom5.TabIndex = 13;
+            this.pic_Nom5.TabStop = false;
+            // 
+            // pic_Nom6
+            // 
+            this.pic_Nom6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_Nom6.Location = new System.Drawing.Point(450, 250);
+            this.pic_Nom6.Name = "pic_Nom6";
+            this.pic_Nom6.Size = new System.Drawing.Size(300, 300);
+            this.pic_Nom6.TabIndex = 14;
+            this.pic_Nom6.TabStop = false;
+            // 
+            // pic_Nom7
+            // 
+            this.pic_Nom7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_Nom7.Location = new System.Drawing.Point(1200, 250);
+            this.pic_Nom7.Name = "pic_Nom7";
+            this.pic_Nom7.Size = new System.Drawing.Size(300, 300);
+            this.pic_Nom7.TabIndex = 15;
+            this.pic_Nom7.TabStop = false;
+            // 
+            // pic_Nom8
+            // 
+            this.pic_Nom8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_Nom8.Location = new System.Drawing.Point(825, 550);
+            this.pic_Nom8.Name = "pic_Nom8";
+            this.pic_Nom8.Size = new System.Drawing.Size(300, 300);
+            this.pic_Nom8.TabIndex = 16;
+            this.pic_Nom8.TabStop = false;
+            // 
+            // lbl_Nom1
+            // 
+            this.lbl_Nom1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Nom1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Nom1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nom1.Location = new System.Drawing.Point(350, 370);
+            this.lbl_Nom1.Name = "lbl_Nom1";
+            this.lbl_Nom1.Size = new System.Drawing.Size(600, 100);
+            this.lbl_Nom1.TabIndex = 17;
+            this.lbl_Nom1.Text = "Luis Embon Strizzi";
+            this.lbl_Nom1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Nom2
+            // 
+            this.lbl_Nom2.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Nom2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Nom2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nom2.Location = new System.Drawing.Point(1000, 370);
+            this.lbl_Nom2.Name = "lbl_Nom2";
+            this.lbl_Nom2.Size = new System.Drawing.Size(600, 100);
+            this.lbl_Nom2.TabIndex = 18;
+            this.lbl_Nom2.Text = "label1";
+            this.lbl_Nom2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Nom3
+            // 
+            this.lbl_Nom3.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Nom3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Nom3.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nom3.Location = new System.Drawing.Point(350, 850);
+            this.lbl_Nom3.Name = "lbl_Nom3";
+            this.lbl_Nom3.Size = new System.Drawing.Size(600, 100);
+            this.lbl_Nom3.TabIndex = 19;
+            this.lbl_Nom3.Text = "label1";
+            this.lbl_Nom3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Nom4
+            // 
+            this.lbl_Nom4.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Nom4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Nom4.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nom4.Location = new System.Drawing.Point(1000, 850);
+            this.lbl_Nom4.Name = "lbl_Nom4";
+            this.lbl_Nom4.Size = new System.Drawing.Size(600, 100);
+            this.lbl_Nom4.TabIndex = 20;
+            this.lbl_Nom4.Text = "label1";
+            this.lbl_Nom4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Nom5
+            // 
+            this.lbl_Nom5.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Nom5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Nom5.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nom5.Location = new System.Drawing.Point(650, 550);
+            this.lbl_Nom5.Name = "lbl_Nom5";
+            this.lbl_Nom5.Size = new System.Drawing.Size(600, 100);
+            this.lbl_Nom5.TabIndex = 21;
+            this.lbl_Nom5.Text = "label1";
+            this.lbl_Nom5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Nom6
+            // 
+            this.lbl_Nom6.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Nom6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Nom6.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nom6.Location = new System.Drawing.Point(300, 550);
+            this.lbl_Nom6.Name = "lbl_Nom6";
+            this.lbl_Nom6.Size = new System.Drawing.Size(600, 100);
+            this.lbl_Nom6.TabIndex = 22;
+            this.lbl_Nom6.Text = "label1";
+            this.lbl_Nom6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Nom7
+            // 
+            this.lbl_Nom7.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Nom7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Nom7.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nom7.Location = new System.Drawing.Point(1050, 550);
+            this.lbl_Nom7.Name = "lbl_Nom7";
+            this.lbl_Nom7.Size = new System.Drawing.Size(600, 100);
+            this.lbl_Nom7.TabIndex = 23;
+            this.lbl_Nom7.Text = "label1";
+            this.lbl_Nom7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Nom8
+            // 
+            this.lbl_Nom8.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Nom8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Nom8.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nom8.Location = new System.Drawing.Point(675, 850);
+            this.lbl_Nom8.Name = "lbl_Nom8";
+            this.lbl_Nom8.Size = new System.Drawing.Size(600, 100);
+            this.lbl_Nom8.TabIndex = 24;
+            this.lbl_Nom8.Text = "label1";
+            this.lbl_Nom8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormGruposAmigos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.BackgroundImage = global::Pantalla_Contraseña.Properties.Resources.Fondo;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.lbl_Nom8);
+            this.Controls.Add(this.lbl_Nom7);
+            this.Controls.Add(this.lbl_Nom6);
+            this.Controls.Add(this.lbl_Nom5);
+            this.Controls.Add(this.lbl_Nom4);
+            this.Controls.Add(this.lbl_Nom3);
+            this.Controls.Add(this.lbl_Nom2);
+            this.Controls.Add(this.lbl_Nom1);
+            this.Controls.Add(this.pic_Nom8);
+            this.Controls.Add(this.pic_Nom7);
+            this.Controls.Add(this.pic_Nom6);
+            this.Controls.Add(this.pic_Nom5);
+            this.Controls.Add(this.pic_Nom4);
+            this.Controls.Add(this.pic_Nom3);
+            this.Controls.Add(this.pic_Nom2);
+            this.Controls.Add(this.pic_Nom1);
+            this.Name = "FormGruposAmigos";
+            this.Text = "FormGruposAmigos";
+            this.Load += new System.EventHandler(this.FormGruposAmigos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Nom8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_Nom1;
+        private System.Windows.Forms.PictureBox pic_Nom2;
+        private System.Windows.Forms.PictureBox pic_Nom3;
+        private System.Windows.Forms.PictureBox pic_Nom4;
+        private System.Windows.Forms.PictureBox pic_Nom5;
+        private System.Windows.Forms.PictureBox pic_Nom6;
+        private System.Windows.Forms.PictureBox pic_Nom7;
+        private System.Windows.Forms.PictureBox pic_Nom8;
+        private System.Windows.Forms.Label lbl_Nom1;
+        private System.Windows.Forms.Label lbl_Nom2;
+        private System.Windows.Forms.Label lbl_Nom3;
+        private System.Windows.Forms.Label lbl_Nom4;
+        private System.Windows.Forms.Label lbl_Nom5;
+        private System.Windows.Forms.Label lbl_Nom6;
+        private System.Windows.Forms.Label lbl_Nom7;
+        private System.Windows.Forms.Label lbl_Nom8;
+    }
+}
